e68fef7a3b51c1c650340c669a7309c618b69f82
Nathan Fonseca <airide101@gmail.com>
2019-01-17 23:58:10 -0500

    Added cross platform input and ship X movement.

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m/s")] [SerializeField] float xSpeed = 5f;
+    [Tooltip("In m")] [SerializeField] float xRange = 5f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffsetPerFrame = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffsetPerFrame;
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
