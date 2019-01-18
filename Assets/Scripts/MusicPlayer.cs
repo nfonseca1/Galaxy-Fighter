fa9e87805d3df672dbfe7943988faaa9cf617117
Nathan Fonseca <airide101@gmail.com>
2019-01-18 16:48:35 -0500

    Created seperate music player and music player script for ensuring one player at a time.

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    void Awake()
+    {
+        int numberOfMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        if (numberOfMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -5,11 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreen : MonoBehaviour
 {
-    void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
