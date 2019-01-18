8c771666020056b44ca848ca2d4ebb42bee9a7c7
Nathan Fonseca <airide101@gmail.com>
2019-01-18 17:07:31 -0500

    Added particle collision. Enemy script blows up ship when shot.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathFX;
+
+    MeshRenderer meshRenderer;
+    Collider collider;
+
+    float destroyDelay = 1.5f;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        collider = GetComponent<Collider>();
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        deathFX.SetActive(true);
+        meshRenderer.enabled = false;
+        collider.enabled = false;
+        Invoke("Destroy", destroyDelay);
+    }
+
+    private void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
