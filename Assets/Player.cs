18ebdacf5c2c341eb313b41ab0049506c0f9215f
Nathan Fonseca <airide101@gmail.com>
2019-01-18 00:10:30 -0500

    Y axis movement setup.

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,11 +5,19 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In m/s")] [SerializeField] float xSpeed = 5f;
+    [Tooltip("In m/s")] [SerializeField] float xSpeed = 10f;
     [Tooltip("In m")] [SerializeField] float xRange = 5f;
-    
+    [Tooltip("In m/s")] [SerializeField] float ySpeed = 10f;
+    [Tooltip("In m")] [SerializeField] float yRange = 3f;
+
     // Update is called once per frame
     void Update()
+    {
+        MoveOnXAxis();
+        MoveOnYAxis();
+    }
+
+    private void MoveOnXAxis()
     {
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffsetPerFrame = xThrow * xSpeed * Time.deltaTime;
@@ -17,5 +25,15 @@ public class Player : MonoBehaviour
         float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
 
         transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+
+    private void MoveOnYAxis()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffsetPerFrame = yThrow * ySpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffsetPerFrame;
+        float clampedYPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
     }
 }
