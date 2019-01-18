cd5a5d6f891bc6e9484d8ce61cdfdab804814747
Nathan Fonseca <airide101@gmail.com>
2019-01-18 17:42:10 -0500

    Modified enemy script to instantiate explosion rather than activate it as a child.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,35 +4,25 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] Transform parent;
     [SerializeField] GameObject deathFX;
-
-    MeshRenderer meshRenderer;
-    BoxCollider boxCollider;
-
-    float destroyDelay = 1.5f;
 
     void Start()
     {
         AddNonTriggerBoxCollider();
-        meshRenderer = GetComponent<MeshRenderer>();
     }
 
     private void AddNonTriggerBoxCollider()
     {
-        boxCollider = gameObject.AddComponent<BoxCollider>();
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
         boxCollider.isTrigger = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
-        deathFX.SetActive(true);
-        meshRenderer.enabled = false;
-        boxCollider.enabled = false;
-        Invoke("Destroy", destroyDelay);
-    }
-
-    private void Destroy()
-    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(fx, 2f);
         Destroy(gameObject);
     }
 }
