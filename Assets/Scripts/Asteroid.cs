29146b106fccb1786521db9189b7ce6434f9d6e8
Nathan Fonseca <airide101@gmail.com>
2019-01-30 19:04:08 -0500

    Added new scene and created asteroid and emitter scripts.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    Vector3 speed;
+    float zSpeed = 5f;
+    float xSpeed = 0;
+    float ySpeed = 0;
+
+    [SerializeField] float minXSpeed = -10;
+    [SerializeField] float maxXSpeed = 10;
+    [SerializeField] float minYSpeed = -10;
+    [SerializeField] float maxYSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xSpeed = Random.Range(minXSpeed, maxXSpeed);
+        ySpeed = Random.Range(minYSpeed, maxYSpeed);
+        speed = new Vector3(xSpeed, ySpeed, zSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + transform.forward + speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+    [SerializeField] Transform asteroid;
+    Quaternion rotation;
+    float time = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        
+        if(time >= 1f)
+        {
+            time = 0;
+            
+            Instantiate(asteroid, transform.position, transform.rotation);
+        }
+    }
+}
